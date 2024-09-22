f2fef89d15235336e7b776118f33f773c1c438d6
nhadi44 <hadinurhidayat97@hotmail.com>
2024-09-23 00:22:05 +0700

    modify sitemap

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -2,17 +2,11 @@ export async function GET() {
   const baseUrl = "https://www.hadinurhidayat.cloud";
   const pages = [
     { path: "/", lastmod: "2024-09-22", changefreq: "daily", priority: "1.0" },
-    // {
-    //   path: "/about",
-    //   lastmod: "2024-09-22",
-    //   changefreq: "monthly",
-    //   priority: "0.8",
-    // },
-    // Add more pages as needed
+    // Tambahkan halaman lain jika perlu
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-url/1.0">
         ${pages
           .map(({ path, lastmod, changefreq, priority }) => {
             return `
