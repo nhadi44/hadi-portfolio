032e4c65988a746966952a850bfee9440490bc96
nhadi44 <hadinurhidayat97@hotmail.com>
2024-10-03 16:40:41 +0700

    fixing error

--- src/app/components/about/aboutLeftContent.tsx
+++ src/app/components/about/aboutLeftContent.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-
 export const AboutLeftContent = () => {
   return (
     <div className="col-span-6 flex items-center">
       <div>
         <h5>About Me</h5>
         <h1>Who am I?</h1>
-        <h3>I'm Hadi Nurhidayat, a web developer and tech enthusiast.</h3>
+        <h3>I&aposm Hadi Nurhidayat, a web developer and tech enthusiast.</h3>
         <p>
-          I'm a website developer based in Jakarta, Indonesia, with over 3 years of hands-on experience. I specialize in
-          building high-performance, user-focused websites using the latest and most popular web technologies. My
-          passion
-          lies in creating seamless digital experiences that not only look great but also deliver optimal performance,
-          ensuring a smooth and engaging user experience.
+          I&aposm a website developer based in Jakarta, Indonesia, with over 3
+          years of hands-on experience. I specialize in building
+          high-performance, user-focused websites using the latest and most
+          popular web technologies. My passion lies in creating seamless digital
+          experiences that not only look great but also deliver optimal
+          performance, ensuring a smooth and engaging user experience.
         </p>
       </div>
     </div>
